36a46253d425db851403200f359a671a9795addb
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-06-05 22:56:53 +0530

    Use average operations

--- queries.sql
+++ queries.sql
@@ -49,3 +49,15 @@ UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 1;
 -- Commit transaction
 COMMIT;
 
+-- How many animals are there?
+SELECT count(*) FROM animals;
+-- How many animals have never tried to escape?
+SELECT count(*) FROM animals WHERE escape_attempts = 0;
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered,MAX(escape_attempts) FROM animals GROUP BY neutered;
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-12-31' AND '2000-12-31' GROUP BY species;

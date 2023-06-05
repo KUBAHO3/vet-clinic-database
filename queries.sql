ebb216f95ef2aea5752c9b0ff00be77a4be763c9
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-06-05 22:50:54 +0530

    Use savepoint and commit

--- queries.sql
+++ queries.sql
@@ -21,3 +21,31 @@ select * from animals where weight_kg>=10.4 AND weight_kg<=17.3;
 BEGIN;
 UPDATE animals SET species = 'unspecified';
 ROLLBACK;
+
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+UPDATE animals SET species = 'pokemon' WHERE species = '';
+-- Commit the transaction.
+COMMIT;
+-- Inside a transaction delete all records in the animals table, then roll back the transaction.
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+-- STARTNG A TRANSACTION:
+BEGIN;
+-- Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+-- Creating a savepoint for the transaction.
+SAVEPOINT savepoint_1;
+-- Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1;
+-- Rollback to the savepoint
+ROLLBACK TO SAVEPOINT savepoint_1;
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 1;
+-- Commit transaction
+COMMIT;
+
